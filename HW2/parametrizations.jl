6511e0f12aafbb78f9d3ba1f69cc0e2c654f9fc4
Timon <timonbub@gmail.com>
2026-06-05 14:02:54 +0200

    Finished project

---
+++ HW2/parametrizations.jl
@@ -0,0 +1,96 @@
+#!!!par 1
+p1(t) = [cos(4t)*cos(t), cos(6t)*sin(t)]
+pdot1(t) = [-4sin(4t)*cos(t) - cos(3t)*sin(t), -6sin(6t)*sin(t) + cos(6t)*cos(t)]
+intp1 = [0, pi]
+q1(u) = [0.8*cos(u), 0.4*sin(u)]
+qdot1(u) = [-0.8*sin(u), 0.4*cos(u)]
+intq1 = [0, 2pi]
+h = 0.2
+#!!!par 2
+p2(t) = [cos(4t)*cos(t), cos(6t)*sin(t)]
+pdot2(t) = [-4sin(4t)*cos(t) - cos(4t)*sin(t), -6sin(6t)*sin(t) + cos(6t)*cos(t)]
+intp2 = [0, pi]
+q2(u) = [0.6*cos(u), 0.4*sin(u)]
+qdot2(u) = [-0.6*sin(u), 0.4*cos(u)]
+intq2 = [0, 2pi]
+h = 0.2
+#!!!par 3
+p3(t) = [cos(4t)*cos(t), cos(6t)*sin(t)]
+pdot3(t) = [-4sin(4t)*cos(t) - cos(4t)*sin(t), -6sin(6t)*sin(t) + cos(6t)*cos(t)]
+intp3 = [0, pi]
+q3(u) = [0.4*cos(u), 0.4*sin(u)]
+qdot3(u) = [-0.4*sin(u), 0.4*cos(u)]
+intq3 = [0, 2pi]
+h = 0.2
+#!!!par 4
+p4(t) = [cos(4t)*cos(t), cos(6t)*sin(t)]
+pdot4(t) = [-4sin(4t)*cos(t) - cos(4t)*sin(t), -6sin(6t)*sin(t) + cos(6t)*cos(t)]
+intp4 = [0, pi]
+q4(u) = [0.2*cos(u), 0.4*sin(u)]
+qdot4(u) = [-0.2*sin(u), 0.4*cos(u)]
+intq4 = [0, 2pi]
+h = 0.2
+#!!!par 5
+p5(t) = [cos(4t)*cos(t), cos(7t)*sin(t)]
+pdot5(t) = [-4sin(4t)*cos(t) - cos(4t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp5 = [0, pi]
+q5(u) = [0.8*cos(u), 0.4*sin(u)]
+qdot5(u) = [-0.8*sin(u), 0.4*cos(u)]
+intq5 = [0, 2pi]
+h = 0.2
+#!!!par 6
+p6(t) = [cos(4t)*cos(t), cos(7t)*sin(t)]
+pdot6(t) = [-4sin(4t)*cos(t) - cos(4t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp6 = [0, pi]
+q6(u) = [0.6*cos(u), 0.4*sin(u)]
+qdot6(u) = [-0.6*sin(u), 0.4*cos(u)]
+intq6 = [0, 2pi]
+h = 0.2
+#!!!par 7
+p7(t) = [cos(4t)*cos(t), cos(7t)*sin(t)]
+pdot7(t) = [-4sin(4t)*cos(t) - cos(4t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp7 = [0, pi]
+q7(u) = [0.4*cos(u), 0.4*sin(u)]
+qdot7(u) = [-0.4*sin(u), 0.4*cos(u)]
+intq7 = [0, 2pi]
+h = 0.2
+#!!!par 8
+p8(t) = [cos(4t)*cos(t), cos(7t)*sin(t)]
+pdot8(t) = [-4sin(4t)*cos(t) - cos(4t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp8 = [0, pi]
+q8(u) = [0.3*cos(u), 0.5*sin(u)]
+qdot8(u) = [-0.3*sin(u), 0.5*cos(u)]
+intq8 = [0, 2pi]
+h = 0.2
+#!!!par 9
+p9(t) = [cos(6t)*cos(t), cos(7t)*sin(t)]
+pdot9(t) = [-6sin(6t)*cos(t) - cos(6t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp9 = [0, pi]
+q9(u) = [0.3*cos(u), 0.5*sin(u)]
+qdot9(u) = [-0.3*sin(u), 0.5*cos(u)]
+intq9 = [0, 2pi]
+h = 0.2
+#!!!par 10
+p10(t) = [cos(6t)*cos(t), cos(7t)*sin(t)]
+pdot10(t) = [-6sin(6t)*cos(t) - cos(6t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp10 = [0, pi]
+q10(u) = [0.5*cos(u), 0.6*sin(u)]
+qdot10(u) = [-0.5*sin(u), 0.6*cos(u)]
+intq10 = [0, 2pi]
+h = 0.2
+#!!!par 11
+p11(t) = [cos(6t)*cos(t), cos(7t)*sin(t)]
+pdot11(t) = [-6sin(6t)*cos(t) - cos(6t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp11 = [0, pi]
+q11(u) = [0.7*cos(u), 0.6*sin(u)]
+qdot11(u) = [-0.7*sin(u), 0.6*cos(u)]
+intq11 = [0, 2pi]
+h = 0.2
+#!!!par 12
+p12(t) = [cos(6t)*cos(t), cos(7t)*sin(t)]
+pdot12(t) = [-6sin(6t)*cos(t) - cos(6t)*sin(t), -7sin(7t)*sin(t) + cos(7t)*cos(t)]
+intp12 = [0, pi]
+q12(u) = [0.3*cos(u), 0.6*sin(u)]
+qdot12(u) = [-0.3*sin(u), 0.6*cos(u)]
+intq12 = [0, 2pi]
+h = 0.2
